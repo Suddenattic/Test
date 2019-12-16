5c933d65500cb42190ae8cc1e146eac7ce754cfa
kagel <LuckKage@gmail.com>
2019-12-16 14:05:16 +0300

    Два теста

---
+++ src/com/pack/test/AnotherTestGit.java
@@ -0,0 +1,7 @@
+package com.pack.test;
+
+public class AnotherTestGit {
+    public static void main(String[] args) {
+        System.out.println("AnotherTest");
+    }
+}
---
+++ src/com/pack/test/TestGit.java
@@ -0,0 +1,7 @@
+package com.pack.test;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
